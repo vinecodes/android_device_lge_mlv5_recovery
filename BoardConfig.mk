632e2a194ae3ca8df7d7ae9e6b76131b66840335
Vineeth Penugonda <vineeth.pen@outlook.com>
2017-08-24 16:31:24 +0530

    Added few flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@ DEVICE_PATH := device/lge/mlv5
 
 # Platform
 TARGET_BOARD_PLATFORM := mt6750
-TARGET_BOARD_PLATFORM_GPU := mali-t860
+TARGET_BOARD_PLATFORM_GPU := mali-t860mp2
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := mt6755
@@ -28,11 +28,20 @@ TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
 # Architecture
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_VFP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_CORTEX_A53 := true
+TARGET_CPU_SMP := true
+
+# MTK Hardware
+BOARD_HAS_MTK_HARDWARE := true
+MTK_HARDWARE := true
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
