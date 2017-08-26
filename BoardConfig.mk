32dcfd5baba3dd668483c1ff8cf6196ee4963bc6
Vineeth Penugonda <vineeth.pen@outlook.com>
2017-08-26 14:26:22 +0530

    Fixed BoardConfig.mk
    Signed-off-by: Vineeth Penugonda <vineeth.pen@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,36 +25,26 @@ TARGET_BOOTLOADER_BOARD_NAME := mt6755
 TARGET_NO_BOOTLOADER := true
 
 # Architecture
-ARCH_ARM_HAVE_NEON := true
-ARCH_ARM_HAVE_VFP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_ARCH_VARIANT_CPU := cortex-a53
-TARGET_CPU_ABI_LIST := arm64-v8a,armeabi-v7a,armeabi
-TARGET_CPU_ABI_LIST_32_BIT := armeabi-v7a,armeabi
-TARGET_CPU_ABI_LIST_64_BIT := arm64-v8a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := generic
-TARGET_CPU_SMP := true
 
+TARGET_ARCH := arm64
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := armeabi-v7a
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_VARIANT := generic
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
-TARGET_BOARD_SUFFIX := _64
-TARGET_USES_64_BIT_BINDER := true
+TARGET_CPU_SMP := true
+
+ARCH_ARM_HAVE_NEON := true
+ARCH_ARM_HAVE_VFP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
 
 # Compilation Flags
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-
-
-# MTK Hardware
-BOARD_HAS_MTK_HARDWARE := true
-MTK_HARDWARE := true
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
