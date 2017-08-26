5bff3647bc42357a4aac3e8f9be5fbf578db4d4c
Vineeth Penugonda <vineeth.pen@outlook.com>
2017-08-26 14:57:25 +0530

    Add SMP Flag
    Signed-off-by: Vineeth Penugonda <vineeth.pen@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,6 +38,7 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+TARGET_CPU_SMP := true
 
 ARCH_ARM_HAVE_NEON := true
 ARCH_ARM_HAVE_VFP := true
