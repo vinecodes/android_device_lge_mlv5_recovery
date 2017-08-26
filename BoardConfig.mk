c8b28c1fe7f128764b61bc62eabe13103a4acca8
Vineeth Penugonda <vineeth.pen@outlook.com>
2017-08-26 15:07:35 +0530

    Fix OMNI-5.1.1_TWRP CPU2
    TARGET_2ND_CPU_VARIANT on Lollipop should be a 'generic' otherwise it will build error:
    
    collect2: error: ld returned 1 exit status
    make: *** [out/target/product/mlv5/obj_arm/SHARED_LIBRARIES/libc_intermediates/LINKED/libc.so] Error 1
    
    For Android 6.0 can be used 'cortex-a53'
    
    Signed-off-by: Vineeth Penugonda <vineeth.pen@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,18 +25,19 @@ TARGET_BOOTLOADER_BOARD_NAME := mt6755
 TARGET_NO_BOOTLOADER := true
 
 # Architecture
-
+# For 64 bit
 TARGET_ARCH := arm64
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 := armeabi-v7a
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_VARIANT := generic
 
+# For 32 bit
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_VARIANT := generic
 
 TARGET_CPU_SMP := true
 
